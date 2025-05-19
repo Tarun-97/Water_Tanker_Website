c6b16e2ee22f80969f16d8abc41e6c806cecfb5c
Tarun-97 <62595855+Tarun-97@users.noreply.github.com>
2025-05-19 18:53:27 +0530

    Update behind.php

--- behind.php
+++ behind.php
@@ -27,19 +27,18 @@
 <body style="background-color: rgb(22, 66, 119);">
     
     <form onsubmit="return validateForm()" action="insert.php">
-        <h2 align="center">BOOKING CENTRE</h2>
+        <h2 align="center">BOOKING PAGE</h2>
         <table>
             <tr><th>Name:</th><td><input type="text" placeholder="Enter your name" name=name required /></td></tr>
             <tr><th>Phone:</th><td><input type="text" id="phone" placeholder="+91XXXXXXXXXX" name=ph required /></td></tr>
             <tr><th>Email:</th><td><input type="text" id="email" placeholder="e.g. abc@mail.com" name=eml required /></td></tr>
-            <tr><th>Date of Birth:</th><td><input type="date" name=dob required /></td></tr>
             <tr><th>Gender:</th>
                 <td>
                     <input type="radio" name=g value=Male required />Male
                     <input type="radio" name=g value=Female required />Female
                 </td>
             </tr>
-            <tr><th>Water Tanker</th>
+            <tr><th>Water Tanker:</th>
                 <td>
                     <select name=s required>
                         <option value="">--Select--</option>
@@ -68,14 +67,13 @@
             <tr><th>Reviews:</th><td><textarea rows="3" cols="20" name=rvs required></textarea></td></tr>
             <tr><td colspan="2" align="center"><input type="Submit" /></td></tr>
             <tr><td colspan="2" align="center"><input type="Reset" /></td></tr>
-            <tr><th>Start Time:</th><td  onload="showdate()" required>
-            
-	<?php
-		date_default_timezone_set("Asia/Kolkata");
-		echo '<p id="server-date"> '.date("h:i:sa").'</p>' ;
+            <tr><th>Date & Time:</th><td>
+                <?php
+    date_default_timezone_set("Asia/Kolkata");
+    echo '<p id="server-date">' . date("d-m-Y") . ' ' . date("h:i:sa") . '</p>';
 
-	?>
-        </td>
+    ?>
+    </td>
     </tr>
         </table>
     </form>
